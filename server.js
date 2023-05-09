6dcb0dcc99353f36cdfc9e72e27802968fa71c58
CoColate <c@cocolate.cc>
2023-05-09 17:00:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,25 +19,27 @@ export default function proxyServer(proxyUrl, port, onListening) {
   const handleRssFeed = (req, res) => {
     if (sk != req.query.sk) {
       res.status(500).send("sk not allowed");
-    }
-    // 获取用户传入的rss订阅链接
-    const rssUrl = req.query.url;
+    }else{
+      
+      // 获取用户传入的rss订阅链接
+      const rssUrl = req.query.url;
 
-    console.log("handle rss: ", rssUrl);
+      console.log("handle rss: ", rssUrl);
 
-    // 判断rss订阅链接是否存在
-    if (!rssUrl) {
-      // 如果不存在，返回错误信息
-      res.status(400).send('Please provide a valid rss feed url.');
-    } else {
-      // 如果存在，使用http-proxy转发请求到rss订阅链接，并将响应返回给用户
-      fetch(rssUrl, param).then(rssRes => rssRes.text()).then((rssText) => {
-        res.status(200).send(rssText);
-      }).catch(e => {
-        console.log(e.toString())
-        res.status(500).send(e.toString());
-      })
+      // 判断rss订阅链接是否存在
+      if (!rssUrl) {
+        // 如果不存在，返回错误信息
+        res.status(400).send('Please provide a valid rss feed url.');
+      } else {
+        // 如果存在，使用http-proxy转发请求到rss订阅链接，并将响应返回给用户
+        fetch(rssUrl, param).then(rssRes => rssRes.text()).then((rssText) => {
+          res.status(200).send(rssText);
+        }).catch(e => {
+          console.log(e.toString())
+          res.status(500).send(e.toString());
+        })
 
+      }
     }
   };
 
