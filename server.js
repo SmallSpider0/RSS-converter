5b62c1aaebe71a37305627335fd223620dae4988
SmallSpider0 <568442079@qq.com>
2023-05-09 17:10:49 +0800

    fix bug

--- server.js
+++ server.js
@@ -48,24 +48,6 @@ export default function proxyServer(proxyUrl, port, onListening) {
 
       }
     }
-    // 获取用户传入的rss订阅链接
-    const rssUrl = req.query.url;
-
-    // console.log("handle rss: ", rssUrl);
-
-    // 判断rss订阅链接是否存在
-    if (!rssUrl) {
-      // 如果不存在，返回错误信息
-      res.status(400).send('Please provide a valid rss feed url.');
-    } else {
-      // 如果存在，使用http-proxy转发请求到rss订阅链接，并将响应返回给用户
-      fetch(rssUrl, param).then(rssRes => rssRes.text()).then((rssText) => {
-        res.status(200).send(rssText);
-      }).catch(e => {
-        console.log(e.toString())
-        res.status(500).send(e.toString());
-      })
-    }
   };
 
   // 定义一个路由，用于处理用户的get请求
