9b2838db89eefed4d2c448e5d86a8aad666408e4
CoColate <c@cocolate.cc>
2023-05-09 08:10:50 +0000

    main feature

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-import Server from "./server.js";
+import http from "http";
+import fetch from 'node-fetch'
+import proxyServer from "./server.js";
 
-let url = 'https://www.dmhy.org/topics/rss/rss.xml?keyword=%E4%B8%BA%E7%BE%8E%E5%A5%BD%E7%9A%84%E4%B8%96%E7%95%8C%E7%8C%AE%E4%B8%8A%E7%88%86%E7%84%B0'
+let server = await new Promise((res, rej) => {
+    let proxy = "http://127.0.0.1:7890"
+    let server = proxyServer(proxy, 3000, () => res());
+    return server;
+})
 
-let server = new Server()
-let res = await server.getData(url)
-console.log(res)
+let url = 'https://www.dmhy.org/topics/rss/rss.xml?keyword=为美好的世界献上爆焰'
+
+let rr = await fetch(url);
+fetch(url).then(res => {
+    console.log("test:", res.body);
+})
+
+fetch(`http://127.0.0.1:3000/?url=${encodeURIComponent(url)}&sk=${"sksksk"}`).then(res => res.text()).then(text => {
+    console.log("return:", text);
+}).catch(e => {
+    console.log("post err: ", e);
+})
+
--- server.js
+++ server.js
@@ -1,57 +1,53 @@
 import express from 'express';
 import fetch from 'node-fetch'
-import {HttpsProxyAgent} from 'https-proxy-agent'
+import { HttpsProxyAgent } from 'https-proxy-agent'
 
-const proxyAddress = 'http://127.0.0.1:7890'
-const agent = new HttpsProxyAgent(proxyAddress)
+const sk = "sksksk"
 
-export default class Server {
+export default function proxyServer(proxyUrl, port, onListening) {
+  const app = express();
 
-  async getData(url){
-    let param = {
-      agent: agent,
-      timeout: 10000,
-      method: 'get'
-    }
-    let response = {}
-    try {
-      response = await fetch(url, param)
-    } catch (error) {
-      logger.error(error.toString())
-      return false
-    }
-    return response
+  // 创建一个http-proxy实例
+
+  let param = {
+    agent: proxyUrl ? new HttpsProxyAgent(proxyUrl) : undefined,
+    timeout: 10000,
+    method: 'get'
   }
 
-  start(){
-    // 创建一个express应用
-    const app = express();
+  // 定义一个中间件函数，用于处理用户传入的rss订阅链接
+  const handleRssFeed = (req, res) => {
+    if (sk != req.query.sk) {
+      res.status(500).send("sk not allowed");
+    }
+    // 获取用户传入的rss订阅链接
+    const rssUrl = req.query.url;
 
-    // 创建一个http-proxy实例
-    const proxy = httpProxy.createProxyServer({});
+    console.log("handle rss: ", rssUrl);
 
-    // 定义一个中间件函数，用于处理用户传入的rss订阅链接
-    const handleRssFeed = (req, res) => {
-      // 获取用户传入的rss订阅链接
-      const rssUrl = req.query.rss;
+    // 判断rss订阅链接是否存在
+    if (!rssUrl) {
+      // 如果不存在，返回错误信息
+      res.status(400).send('Please provide a valid rss feed url.');
+    } else {
+      // 如果存在，使用http-proxy转发请求到rss订阅链接，并将响应返回给用户
+      fetch(rssUrl, param).then(rssRes => rssRes.text()).then((rssText) => {
+        res.status(200).send(rssText);
+      }).catch(e => {
+        console.log(e.toString())
+        res.status(500).send(e.toString());
+      })
 
-      // 判断rss订阅链接是否存在
-      if (!rssUrl) {
-        // 如果不存在，返回错误信息
-        res.status(400).send('Please provide a valid rss feed url.');
-      } else {
-        // 如果存在，使用http-proxy转发请求到rss订阅链接，并将响应返回给用户
-        proxy.web(req, res, { target: rssUrl });
-      }
-    };
+    }
+  };
 
-    // 定义一个路由，用于处理用户的get请求
-    app.get('/', handleRssFeed);
+  // 定义一个路由，用于处理用户的get请求
+  app.get('/', handleRssFeed);
 
-    // 启动服务器，监听3000端口
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000.');
-    });
-  }
+  // 启动服务器，监听3000端口
+  app.listen(port, () => {
+    console.log('Server is running on port: ', port);
+    onListening();
+  });
+
 }
-
